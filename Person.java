2a5ea0fcaa52fce02c0aa3a5d06f3eccc7452f1f
ArtemZhuravlev <Utah954@gmail.com>
2023-10-24 15:55:03 +0300

    Made a little change in code (rows 2 - 5, 15, 19 - 21)

--- Person.java
+++ Person.java
@@ -1,8 +1,8 @@
 public class Person {
-    String firstName;
-    String lastName;
-    String city;
-    String phoneNumber;
+    private String firstName;
+    private String lastName;
+    private String city;
+    private String phoneNumber;
 
     public Person(String firstName, String lastName, String city, String phoneNumber) {
         this.firstName = firstName;
@@ -12,18 +12,16 @@ public class Person {
     }
 
     public String personInfo() {
-        String info = "Call a citizen " + firstName + " " + lastName +
-                " from town " + city + " by number " + phoneNumber + ".";
-        return info;
+        return String.format("Calling citizen %s %s from %s at %s", firstName, lastName, city, phoneNumber);
     }
 
     public static void main(String[] args) {
-        Person person1 = new Person("Will", "Smith", "New York", "2936729462846");
-        Person person2 = new Person("Jackie", "Chan", "Shanghai", "12312412412");
-        Person person3 = new Person("Sherlock", "Holmes", "London", "37742123513");
+        Person will = new Person("Will", "Smith", "New York", "2936729462846");
+        Person jackie = new Person("Jackie", "Chan", "Shanghai", "12312412412");
+        Person sherlock = new Person("Sherlock", "Holmes", "London", "37742123513");
 
-        System.out.println(person1.personInfo());
-        System.out.println(person2.personInfo());
-        System.out.println(person3.personInfo());
+        System.out.println(will.personInfo());
+        System.out.println(jackie.personInfo());
+        System.out.println(sherlock.personInfo());
     }
 }
